95dd2b38f205936e4e1895a088ede91be4c9434a
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2025-10-14 22:31:29 -0300

    Primeiro commit (Preparando setup do projeto)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { SquareIcon } from '@phosphor-icons/react';
+
+export default function App() {
+  return (
+    <>
+      <p className="text-red-500">
+        <SquareIcon />
+      </p>
+    </>
+  );
+}
